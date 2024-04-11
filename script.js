5081f76f86d9b293efc86d911a7ef3a3057ed654
Jeremy Yang <jeremy.min.yang@gmail.com>
2024-04-11 13:03:31 +0900

    new

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.getElementById("submit").addEventListener("click", function() {
-    const rating = document.getElementById("attractiveness").value;
-    // Send the rating to the server (you'll need backend code for this)
-    // Update user's personalized view based on the rating
-    alert("Rating submitted: " + rating);
-});
